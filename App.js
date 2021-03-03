4a00e8d8a55bdfd32eda9f5c5b02a9aab8aa08d0
samarth767 <63947003+samarth767@users.noreply.github.com>
2021-03-03 21:55:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+  
+import React from 'react';
+//import { AppDrawerNavigator } from './components/AppDrawerNavigator'
+
+
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+
+//const switchNavigator = createSwitchNavigator({
+//})
+
+//const AppContainer =  createAppContainer(switchNavigator);
+
